bc102a3eeea49a0606bba6a973ec5f7026922c72
andersonmoura <anderson.moura@msn.com>
2013-11-18 16:30:48 -0200

    Template
    Adicionado template....

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -10,7 +10,12 @@
 |
 */
 
-
+$hook['display_override'] = array(
+    'class'     => 'Template',
+    'function'  => 'init',
+    'filename'  => 'Template.php',
+    'filepath'  => 'hooks'
+);
 
 /* End of file hooks.php */
 /* Location: ./application/config/hooks.php */
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,6 +19,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->layout = 'dashboard';
 		$this->load->view('welcome_message');
 	}
 }
